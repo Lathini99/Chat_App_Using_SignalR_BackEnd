4327a8a2324546eb1d0cb57b615649359d0fe1e1
Lathini99 <lathidilz2016@gmail.com>
2024-03-05 22:45:13 +0530

    first commit

---
+++ DataService/SharedDb.cs
@@ -0,0 +1,11 @@
+
+
+using System.Collections.Concurrent;
+
+public class SharedDb
+{
+    private readonly ConcurrentDictionary<string, UserConnection> _connections = new ();
+    public ConcurrentDictionary<string, UserConnection> connections => _connections;
+
+
+}
